c701880e66c11fea022b73fd11ad375ac387f95c
Peter Collingridge <peter.collingridge@gmail.com>
2016-10-15 22:16:42 +0100

    Add an about page

--- rango_app/views.py
+++ rango_app/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Front page")
+    return HttpResponse('<h1>Front page</h1><a href="/rango/about">About this site</a>')
+
+
+def about(request):
+    return HttpResponse('<h1>About my app</h1><a href="/rango">Home</a>')
