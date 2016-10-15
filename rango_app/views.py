d74d073602e9aa02ef17a69c8abcfb650918ea19
Peter Collingridge <peter.collingridge@gmail.com>
2016-10-15 22:45:40 +0100

    Create first template view

--- rango_app/views.py
+++ rango_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Front page</h1><a href="/rango/about">About this site</a>')
+    context_dict = {'boldmessage': "Featured content"}
+    return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
