c242e5cefd843df24868a1073be25cfd93138ef7
Peter Collingridge <peter.collingridge@gmail.com>
2016-10-22 16:50:27 +0100

    Add JQuery to base

--- rango_app/views.py
+++ rango_app/views.py
@@ -106,15 +106,16 @@ def add_page(request, category_name_slug):
 
 
 def track_url(request):
-    if 'page_id' in request.GET:
-        page_id = request.GET['page_id']
+    if request.method == 'GET':
+        if 'page_id' in request.GET:
+            page_id = request.GET['page_id']
 
-        try:
-            page = Page.objects.get(id=page_id)
-            page.views += 1
-            page.save()
-            return redirect(page.url)
-        except Page.DoesNotExist:
-            pass
+            try:
+                page = Page.objects.get(id=page_id)
+                page.views += 1
+                page.save()
+                return redirect(page.url)
+            except:
+                pass
 
     return redirect('rango')
