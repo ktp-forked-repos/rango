c41ad4d5c8d6eb5a52f103c839e7ded404ec5406
Peter Collingridge <peter.collingridge@gmail.com>
2016-10-28 19:25:56 +0100

    Redirect to topic page after creating it and show you authored it

--- cms2d/views.py
+++ cms2d/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from cms2d.forms import TopicForm
-from cms2d.models import Topic
+from cms2d.models import Topic, User
 
 
 def home(request):
@@ -16,16 +16,17 @@ def home(request):
 @login_required
 def add_topic(request, topic_name_slug=None):
     if request.method == 'POST':
-        form = TopicForm(request.POST, initial={"author_id": request.user.id + 10})
+        #form = TopicForm(request.POST, initial={"author_id": request.user.id})
+        form = TopicForm(request.POST)
 
         if form.is_valid():
-            form.save(commit=True)
-            return home(request)
+            model = form.save(commit=True)
+            return redirect('topic', topic_name_slug=model.slug)
         else:
             print form.errors
 
     else:
-        form = TopicForm(initial={"author_id": request.user.id + 10})
+        form = TopicForm(initial={"author_id": request.user.id})
 
     return render(request, 'cms2d/add_topic.html', {'form': form})
 
@@ -35,6 +36,13 @@ def topic(request, topic_name_slug):
 
     try:
         topic = Topic.objects.get(slug=topic_name_slug)
+
+        try:
+            author = User.objects.get(pk=topic.author_id)
+            context_dict['author'] = author
+        except User.DoesNotExist:
+            pass
+
         context_dict['topic'] = topic
         context_dict['topic_name'] = topic.name
     except Topic.DoesNotExist:
